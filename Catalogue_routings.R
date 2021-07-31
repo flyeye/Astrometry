0c04befac07e6b2eb7876d9be89ad53a21ed64e5
flyeye <flyeye@users.noreply.github.com>
2021-08-01 00:00:37 +0300

    Update Catalogue_routings.R
    GDR3 added

--- Catalogue_routings.R
+++ Catalogue_routings.R
@@ -11,14 +11,16 @@ tgas_libs_required <- function()
   #library("xtable", lib.loc="~/R/win-library/3.4")
   #library("gridExtra", lib.loc="~/R/win-library/3.4")
   
-  library("tidyverse")
-  library("readxl")
-  library("xlsx")
-  library("stargazer")
-  library("scales")
-  library("gdata")
-  library("xtable")
-  library("gridExtra")
+  require(readr)
+  require(readxl)
+  require(tidyverse)
+  require(xlsx)
+  require(ggplot2)
+  require(scales)
+  require(stargazer)
+  require(gdata)
+  require(xtable)
+  require(gridExtra)
 }
 
 
@@ -451,7 +453,210 @@ get_tyc2sp_OB <- function(tyc2sp_data)
   tyc <- tyc2sp_data %>% filter(PxSp > 0.5) %>% filter(PxSp < 4)  %>% filter( B_V<0 )
   return(tyc)
 }
+# 
+# ===============================================================================
+# --------------------------------   GAIA DR3 Routings  -----------------------------
+# field description: https://gea.esac.esa.int/archive/documentation/GEDR3/Gaia_archive/chap_datamodel/sec_dm_main_tables/ssec_dm_gaia_source.html
+# solution_id,designation,source_id,random_index,ref_epoch,ra,ra_error,dec,dec_error,parallax,parallax_error
+#
+# solution_id : Solution Identifier (long)
+# designation : Unique source designation (unique across all Data Releases) (string)
+# source_id: Unique source identifier (unique within a particular Data Release) (long)
+# random_index : Random index used to select subsets (long)
+# ref_epoch : Reference epoch (double, Time[Julian Years])
+# ra : Right ascension (double, Angle[deg])
+# ra_error : Standard error of right ascension (float, Angle[mas])
+# dec : Declination (double, Angle[deg])
+# dec_error : Standard error of declination (float, Angle[mas])
+# parallax : Parallax (double, Angle[mas] )
+# parallax_error : Standard error of parallax (float, Angle[mas] )
 
+# ___parallax_over_error,pm,pmra,pmra_error,pmdec,pmdec_error,ra_dec_corr,ra_parallax_corr,ra_pmra_corr,ra_pmdec_corr,
+# parallax_over_error : Parallax divided by its standard error (float)
+# pm : Total proper motion (float, Angular Velocity[mas/year])
+# pmra : Proper motion in right ascension direction (double, Angular Velocity[mas/year])
+# pmra_error : Standard error of proper motion in right ascension direction (float, Angular Velocity[mas/year] )
+# pmdec : Proper motion in declination direction (double, Angular Velocity[mas/year] )
+# pmdec_error : Standard error of proper motion in declination direction (float, Angular Velocity[mas/year] )
+# ra_dec_corr : Correlation between right ascension and declination (float, Dimensionless[see description])
+# ra_parallax_corr : Correlation between right ascension and parallax (float, Dimensionless[see description])
+# ra_pmra_corr : Correlation between right ascension and proper motion in right ascension (float, Dimensionless[see description])
+# ra_pmdec_corr : Correlation between right ascension and proper motion in declination (float, Dimensionless[see description])
+
+# ___dec_parallax_corr,dec_pmra_corr,dec_pmdec_corr,parallax_pmra_corr,parallax_pmdec_corr,pmra_pmdec_corr,
+# dec_parallax_corr : Correlation between declination and parallax (float, Dimensionless[see description])
+# dec_pmra_corr : Correlation between declination and proper motion in right ascension (float, Dimensionless[see description])
+# dec_pmdec_corr : Correlation between declination and proper motion in declination (float, Dimensionless[see description])
+# parallax_pmra_corr : Correlation between parallax and proper motion in right ascension (float, Dimensionless[see description])
+# parallax_pmdec_corr : Correlation between parallax and proper motion in declination (float, Dimensionless[see description])
+# pmra_pmdec_corr : Correlation between proper motion in right ascension and proper motion in declination (float, Dimensionless[see description])
+
+# ___astrometric_n_obs_al,astrometric_n_obs_ac,astrometric_n_good_obs_al,astrometric_n_bad_obs_al,astrometric_gof_al,astrometric_chi2_al,
+# astrometric_n_obs_al : Total number of observations AL (short)
+# astrometric_n_obs_ac : Total number of observations AC (short)
+# astrometric_n_good_obs_al : Number of good observations AL (short)
+# astrometric_n_bad_obs_al : Number of bad observations AL (short)
+# astrometric_gof_al : Goodness of fit statistic of model wrt along-scan observations (float)
+# astrometric_chi2_al : AL chi-square value (float)
+
+# ____astrometric_excess_noise,astrometric_excess_noise_sig,astrometric_params_solved,astrometric_primary_flag,nu_eff_used_in_astrometry,
+# astrometric_excess_noise : Excess noise of the source (float, Angle[mas])
+# astrometric_excess_noise_sig : Significance of excess noise (float)
+# astrometric_params_solved : Which parameters have been solved for? (byte)
+#   00000112=3	- position
+#   00001112=7	- pos + px
+#   00110112=27	- pos + pm
+#   00111112=31	- pos + px + pm
+#   01111112=63	- pos + px + pm + vr
+#   10111112=95	- pos + px + pm + C
+# astrometric_primary_flag : Primary or seconday (boolean)
+# nu_eff_used_in_astrometry : Effective wavenumber of the source used in the astrometric solution
+
+# __pseudocolour,pseudocolour_error,ra_pseudocolour_corr,dec_pseudocolour_corr,parallax_pseudocolour_corr,pmra_pseudocolour_corr,pmdec_pseudocolour_corr,
+# pseudocolour : Astrometrically estimated pseudocolour of the source (float, Misc[??m???1])
+# pseudocolour_error : Standard error of the pseudocolour of the source (float, Misc[??m???1])
+# ra_pseudocolour_corr : Correlation between right ascension and pseudocolour (float, Dimensionless[see description])
+# dec_pseudocolour_corr : Correlation between declination and pseudocolour (float, Dimensionless[see description])
+# parallax_pseudocolour_corr : Correlation between parallax and pseudocolour (float, Dimensionless[see description])
+# pmra_pseudocolour_corr : Correlation between proper motion in right asension and pseudocolour (float, Dimensionless[see description])
+# pmdec_pseudocolour_corr : Correlation between proper motion in declination and pseudocolour (float, Dimensionless[see description])
+
+# ___astrometric_matched_transits,visibility_periods_used,astrometric_sigma5d_max,matched_transits,new_matched_transits,matched_transits_removed,
+# astrometric_matched_transits : Matched FOV transits used in the AGIS solution (short)
+# visibility_periods_used : Number of visibility periods used in Astrometric solution (short)
+# astrometric_sigma5d_max : The longest semi-major axis of the 5-d error ellipsoid (float, Angle[mas])
+# matched_transits : The number of transits matched to this source (short)
+# new_matched_transits : The number of transits newly incorporated into an existing source in the current cycle (short)
+# matched_transits_removed : The number of transits removed from an existing source in the current cycle (short)
+
+# ___ipd_gof_harmonic_amplitude,ipd_gof_harmonic_phase,ipd_frac_multi_peak,ipd_frac_odd_win,ruwe,
+# ipd_gof_harmonic_amplitude : Amplitude of the IPD GoF versus position angle of scan (float, Dimensionless[see description])
+# ipd_gof_harmonic_phase : Phase of the IPD GoF versus position angle of scan (float, Angle[deg])
+# ipd_frac_multi_peak : Percent of successful-IPD windows with more than one peak (byte)
+# ipd_frac_odd_win : Percent of transits with truncated windows or multiple gate (byte)
+# ruwe : Renormalised unit weight error (float)
+
+# ___scan_direction_strength_k1,scan_direction_strength_k2,scan_direction_strength_k3,scan_direction_strength_k4,scan_direction_mean_k1,scan_direction_mean_k2,scan_direction_mean_k3,scan_direction_mean_k4,
+# scan_direction_strength_k1 : Degree of concentration of scan directions across the source (float)
+# scan_direction_strength_k2 : Degree of concentration of scan directions across the source (float)
+# scan_direction_strength_k3 : Degree of concentration of scan directions across the source (float)
+# scan_direction_strength_k4 : Degree of concentration of scan directions across the source (float)
+# scan_direction_mean_k1 : Mean position angle of scan directions across the source (float, Angle[deg])
+# scan_direction_mean_k2 : Mean position angle of scan directions across the source (float, Angle[deg])
+# scan_direction_mean_k3 : Mean position angle of scan directions across the source (float, Angle[deg])
+# scan_direction_mean_k4 : Mean position angle of scan directions across the source (float, Angle[deg])
+
+# __duplicated_source,phot_g_n_obs,phot_g_mean_flux,phot_g_mean_flux_error,phot_g_mean_flux_over_error,phot_g_mean_mag,
+# duplicated_source : Source with multiple source identifiers (boolean)
+# phot_g_n_obs : Number of observations contributing to G photometry (short)
+# phot_g_mean_flux : G-band mean flux (double, Flux[e-/s])
+# phot_g_mean_flux_error : Error on G-band mean flux (float, Flux[e-/s])
+# phot_g_mean_flux_over_error : G-band mean flux divided by its error (float)
+# phot_g_mean_mag : G-band mean magnitude (float, Magnitude[mag])
+
+# __phot_bp_n_obs,phot_bp_mean_flux,phot_bp_mean_flux_error,phot_bp_mean_flux_over_error,phot_bp_mean_mag,phot_rp_n_obs,phot_rp_mean_flux,phot_rp_mean_flux_error,phot_rp_mean_flux_over_error,phot_rp_mean_mag,
+# phot_bp_n_obs : Number of observations contributing to BP photometry (short)
+# phot_bp_mean_flux : Integrated BP mean flux (double, Flux[e-/s])
+# phot_bp_mean_flux_error : Error on the integrated BP mean flux (float, Flux[e-/s])
+# phot_bp_mean_flux_over_error : Integrated BP mean flux divided by its error (float)
+# phot_bp_mean_mag : Integrated BP mean magnitude (float, Magnitude[mag])
+# phot_rp_n_obs : Number of observations contributing to RP photometry (short)
+# phot_rp_mean_flux : Integrated RP mean flux (double, Flux[e-/s])
+# phot_rp_mean_flux_error : Error on the integrated RP mean flux (float, Flux[e-/s])
+# phot_rp_mean_flux_over_error : Integrated RP mean flux divided by its error (float)
+# phot_rp_mean_mag : Integrated RP mean magnitude (float, Magnitude[mag])
+
+# __phot_bp_n_contaminated_transits,phot_bp_n_blended_transits,phot_rp_n_contaminated_transits,phot_rp_n_blended_transits,phot_proc_mode,
+# phot_bp_n_contaminated_transits : Number of BP contaminated transits (short)
+# phot_bp_n_blended_transits : Number of BP blended transits (short)
+# phot_rp_n_contaminated_transits : Number of RP contaminated transits (short)
+# phot_rp_n_blended_transits : Number of RP blended transits (short)
+# phot_proc_mode : Photometry processing mode (byte)
+
+# __phot_bp_rp_excess_factor,bp_rp,bp_g,g_rp,dr2_radial_velocity,dr2_radial_velocity_error,dr2_rv_nb_transits,dr2_rv_template_teff,dr2_rv_template_logg,dr2_rv_template_fe_h,
+# phot_bp_rp_excess_factor : BP/RP excess factor (float)
+# bp_rp : BP - RP colour (float, Magnitude[mag])
+# bp_g : BP - G colour (float, Magnitude[mag])
+# g_rp : G - RP colour (float, Magnitude[mag])
+# dr2_radial_velocity : Radial velocity from Gaia DR2 (float, Velocity[km/s] )
+# dr2_radial_velocity_error : Radial velocity error from Gaia DR2 (float, Velocity[km/s] )
+# dr2_rv_nb_transits : Number of transits used to compute radial velocity in Gaia DR2 (short)
+# dr2_rv_template_teff : Teff of the template used to compute radial velocity in Gaia DR2 (float,Temperature[K])
+# dr2_rv_template_logg : logg of the template used to compute radial velocity in Gaia DR2 (float, GravitySurface[log cgs])
+# dr2_rv_template_fe_h : Fe/H of the template used to compute radial velocity in Gaia DR2 (float, Abundances[dex])
+
+# __l,b,ecl_lon,ecl_lat
+# l : Galactic longitude (double, Angle[deg])
+# b : Galactic latitude (double, Angle[deg])
+# ecl_lon : Ecliptic longitude (double, Angle[deg])
+# ecl_lat : Ecliptic latitude (double, Angle[deg])
+
+Gaia_DR3_prepare <- function(path = "D:/Gaia/")
+{
+
+var_types <- "__cidddddddd_dddd____________i_____i____________________________l____d____d____d_________dd_d_ddd__";
+          
+
+var_names <- c("source_id","random_index","ref_epoch","RA","ra_error","DE","dec_error","gPx","parallax_error","parallax_over_error",
+               "gpmRA","pmra_error","gpmDE","pmdec_error","n_good_obs","params","duplicated", "gMag", "bMag", "rMag",
+               "Vr", "Vr_error","Teff","FeH","l","b")
+
+# var_names <- c("solution_id","designation","source_id","random_index","ref_epoch","ra","ra_error","dec","dec_error","parallax","parallax_error",
+#                "parallax_over_error","pm","pmra","pmra_error","pmdec","pmdec_error","ra_dec_corr","ra_parallax_corr","ra_pmra_corr","ra_pmdec_corr",
+#                "dec_parallax_corr","dec_pmra_corr","dec_pmdec_corr","parallax_pmra_corr","parallax_pmdec_corr","pmra_pmdec_corr","astrometric_n_obs_al",
+#                "astrometric_n_obs_ac","astrometric_n_good_obs_al","astrometric_n_bad_obs_al","astrometric_gof_al","astrometric_chi2_al","astrometric_excess_noise",
+#                "astrometric_excess_noise_sig","astrometric_params_solved","astrometric_primary_flag","nu_eff_used_in_astrometry","pseudocolour","pseudocolour_error",
+#                "ra_pseudocolour_corr","dec_pseudocolour_corr","parallax_pseudocolour_corr","pmra_pseudocolour_corr","pmdec_pseudocolour_corr","astrometric_matched_transits",
+#                "visibility_periods_used","astrometric_sigma5d_max","matched_transits","new_matched_transits","matched_transits_removed",
+#                "ipd_gof_harmonic_amplitude","ipd_gof_harmonic_phase","ipd_frac_multi_peak","ipd_frac_odd_win","ruwe","scan_direction_strength_k1",
+#                "scan_direction_strength_k2","scan_direction_strength_k3","scan_direction_strength_k4","scan_direction_mean_k1","scan_direction_mean_k2",
+#                "scan_direction_mean_k3","scan_direction_mean_k4","duplicated_source","phot_g_n_obs","phot_g_mean_flux","phot_g_mean_flux_error",
+#                "phot_g_mean_flux_over_error","phot_g_mean_mag","phot_bp_n_obs","phot_bp_mean_flux","phot_bp_mean_flux_error","phot_bp_mean_flux_over_error",
+#                "phot_bp_mean_mag","phot_rp_n_obs","phot_rp_mean_flux","phot_rp_mean_flux_error","phot_rp_mean_flux_over_error","phot_rp_mean_mag","phot_bp_n_contaminated_transits",
+#                "phot_bp_n_blended_transits","phot_rp_n_contaminated_transits","phot_rp_n_blended_transits","phot_proc_mode","phot_bp_rp_excess_factor",
+#                "bp_rp","bp_g","g_rp","dr2_radial_velocity","dr2_radial_velocity_error","dr2_rv_nb_transits","dr2_rv_template_teff","dr2_rv_template_logg",
+#                "dr2_rv_template_fe_h","l","b","ecl_lon","ecl_lat")
+
+#path <- "Y:/Gaia/"
+#filename <- paste0(path, "GaiaSource_000000-003111.csv.gz")
+#data <- read_csv(filename, col_names = var_names, col_types = var_types, skip = 1)
+#readed <- nrow(data)
+# data2 <- data %>% filter(duplicated==FALSE) %>% filter((params==31) || (params==63) || (params==95)) %>% filter(parallax_over_error>0) %>% filter(parallax_over_error <= 1) %>% filter(gPx>0.2)
+
+  gaia_list <- read_csv(paste0(path, "gaia.list"), col_names = c("filename"), col_types = "c", skip = 0)
+
+
+  readed_all <- 0;
+  filtred_all <-0;
+  
+  gdr3_data <- data.frame()
+
+  for (i in 1:nrow(gaia_list))
+  {
+    gc()
+    filename <- paste0(path, gaia_list$filename[i])
+    cat("step:", i, "file:", filename, "\n")
+    data <- read_csv(filename, col_names = var_names, col_types = var_types, skip = 1)
+    readed <- nrow(data)
+    cat("readed: ", readed, "\n")
+    readed_all <- readed_all + readed
+    cat("Total read:", readed_all, "\n")
+    data2 <- data %>% filter(duplicated==FALSE) %>% filter((params==31) || (params==63) || (params==95)) %>% filter(parallax_over_error>0) %>% filter(parallax_over_error > 5) %>% filter(gPx>0.2)  
+    filtred <- nrow(data2)
+    cat("filtred:", filtred, "\n")
+    filtred_all <- filtred_all +filtred
+    cat("Total filtred:", filtred_all, "\n")
+    if (filtred>0)
+    {
+      data2 <- data2 %>% mutate(RA = RA*pi/180, DE = DE*pi/180)
+      saveRDS(data2, file = paste0(path, tools::file_path_sans_ext(tools::file_path_sans_ext(gaia_list$filename[i])), ".rds"))
+      #write_csv(x = data2, path = paste0(path, "gaia_dr3_good.csv"), append = TRUE, col_names = FALSE)
+    }
+    
+    #gdr3_data <- rbind(gdr3_data, data2)
+  }
+  
+}
 
 # ===============================================================================
 # --------------------------------   TGAS Routings  -----------------------------
